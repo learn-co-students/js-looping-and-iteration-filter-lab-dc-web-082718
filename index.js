18e9a236072a14349bc47d758790d86f68074ca2
drubaby <peryton6@gmail.com>
2018-10-08 16:47:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching (array, string) {
+  return array.filter(function (name) {
+    return name.toLowerCase() === string.toLowerCase()
+  })
+}
+
+function fuzzyMatch(array, partialName) {
+  let lengthOfName = partialName.length
+  return array.filter(function (startsWith) {
+    return startsWith.slice(0, lengthOfName) === partialName
+  })
+}
+
+function matchName(array, string) {
+  return array.filter(function (nameKey) {
+    return nameKey.name === string
+  })
+}
